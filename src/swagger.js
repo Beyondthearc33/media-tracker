bf9b6f5c9bbd42b71e1e0ea266839bb4a2f2d6ab
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-03-14 21:33:27 -0600

    Add initial Swagger configuration for API documentation

---
+++ src/swagger.js
@@ -0,0 +1,22 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Media Tracker API',
+      version: '1.0.0',
+      description: 'API documentation for the Media Tracker project',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+module.exports = swaggerSpec;
