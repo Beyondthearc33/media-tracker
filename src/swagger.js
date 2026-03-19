f6dc381a78d1234b676f96b7e70fa69f3725b056
Angela S. Milford <angela.milford123@gmail.com>
2026-03-18 21:37:30 -1100

    Implementing swagger-autogen using advanced options

--- src/swagger.js
+++ src/swagger.js
@@ -2,10 +2,24 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Description'
+    version: '',            // by default: '1.0.0'
+    title: '',              // by default: 'REST API'
+    description: ''         // by default: ''
   },
-  host: 'localhost:3000'
+  host: '',                 // by default: 'localhost:3000'
+  basePath: '',             // by default: '/'
+  schemes: [],              // by default: ['http']
+  consumes: [],             // by default: ['application/json']
+  produces: [],             // by default: ['application/json']
+  tags: [                   // by default: empty Array
+    {
+      name: '',             // Tag name
+      description: ''       // Tag description
+    },
+    // { ... }
+  ],
+  securityDefinitions: {},  // by default: empty object
+  definitions: {}           // by default: empty object
 };
 
 const outputFile = './swagger-output.json';
