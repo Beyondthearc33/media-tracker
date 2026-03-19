b80a3ba5500f41cf0e1346228dd1416e2f6dfe5b
Angela S. Milford <angela.milford123@gmail.com>
2026-03-18 21:56:42 -1100

    Cleaned up and improved "swagger.js" configuration

--- src/swagger.js
+++ src/swagger.js
@@ -2,39 +2,25 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    version: '',
+    version: '1.0.0',
     title: 'Media Tracker API',
-    description: 'Media tracker will allow users to create a backlog/watchlist of movies/shows they watch. They can track status, rate them, create notes and mark when watched.'         // by default: ''
+    description: 'Media tracker will allow users to create a backlog/watchlist of movies/shows they watch. They can track status, rate them, create notes and mark when watched.'
   },
   host: 'localhost:3000',
   basePath: '/',
   schemes: ['http'],
   consumes: ['application/json'],
   produces: ['application/json'],
-  tags: [                  
-    {
-      name: 'Auth',            
-      description: 'Authentication endpoints'       
-    },
-    {
-        name: 'Media',
-        description: 'Media management'
-    },
-    {
-        name: 'Library',
-        description: 'User media tracking'
-    },
-    {
-        name: 'Collections',
-        description: 'User collections'
-    }
+  tags: [
+    { name: 'Auth', description: 'Authentication endpoints' },
+    { name: 'Media', description: 'Media management' },
+    { name: 'Library', description: 'User media tracking' },
+    { name: 'Collections', description: 'User collections' }
   ]
 };
 
 const outputFile = './src/swagger.json';
-const routes = ['./src/routes/index.js'];
 
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+const routes = ['./src/server.js'];
 
 swaggerAutogen(outputFile, routes, doc);
